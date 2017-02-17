f06e5185f3b3d07f133d0eeb0a5f64546eafb559
Luke Karrys <lukekarrys@gmail.com>
2017-02-16 18:49:08 -0700

    updates

--- index.js
+++ index.js
@@ -1,70 +1,69 @@
-var path = require('path');
-var fs = require('fs');
-var os = require('os');
-
+var path = require('path')
+var fs = require('fs')
+var os = require('os')
 
 function toImports (arr, type) {
-    if (typeof arr === 'undefined') {
-        return '';
-    }
+  if (typeof arr === 'undefined') {
+    return ''
+  }
 
-    if (!Array.isArray(arr)) {
-        arr = [arr];
-    }
-    return (type === 'after' ? os.EOL : '') + arr.map(function (line) {
-        return '@import "' + line + '";';
-    }).join(os.EOL) + (type === 'before' ? os.EOL : '');
+  if (!Array.isArray(arr)) {
+    arr = [arr]
+  }
+  return (type === 'after' ? os.EOL : '') + arr.map(function (line) {
+    return '@import "' + line + '";'
+  }).join(os.EOL) + (type === 'before' ? os.EOL : '')
 }
 
 function addLess (file) {
-    return path.extname(file) === '.less' ? file : file + '.less';
+  return path.extname(file) === '.less' ? file : file + '.less'
 }
 
-function LessImportInserter(options) {
-    options || (options = {});
-    this.before = options.before || {};
-    this.after = options.after || {};
-    this.append = toImports(options.append, 'after');
-    this.lessPath = options.lessPath;
-    this.relativeTo = options.relativeTo;
-    this.less = this.lessPath && !options.less ? fs.readFileSync(this.lessPath, 'utf8') : options.less;
+function LessImportInserter (options) {
+  options || (options = {})
+  this.before = options.before || {}
+  this.after = options.after || {}
+  this.append = toImports(options.append, 'after')
+  this.lessPath = options.lessPath
+  this.relativeTo = options.relativeTo
+  this.less = this.lessPath && !options.less ? fs.readFileSync(this.lessPath, 'utf8') : options.less
 
-    if (!this.less) throw new Error('Use the `path` or `less` option to specify the less file.');
+  if (!this.less) throw new Error('Use the `path` or `less` option to specify the less file.')
 }
 
 LessImportInserter.prototype.build = function () {
-    var res = this.less;
-    var newlineAtEnd = this.less.slice(-1) === os.EOL;
+  var res = this.less
+  var newlineAtEnd = this.less.slice(-1) === os.EOL
 
-    // Change the existing imports to a relative path
-    if (this.relativeTo && this.lessPath) {
-        res = res.replace(/^(@import ")(.*?)(";)$/gm, function (match, g1, g2, g3) {
-            return g1 + path.relative(this.relativeTo, path.dirname(this.lessPath)) + path.sep + g2 + g3;
-        }.bind(this));
-    }
+  // Change the existing imports to a relative path
+  if (this.relativeTo && this.lessPath) {
+    res = res.replace(/^(@import ")(.*?)(";)$/gm, function (match, g1, g2, g3) {
+      return g1 + path.relative(this.relativeTo, path.dirname(this.lessPath)) + path.sep + g2 + g3
+    }.bind(this))
+  }
 
-    res = res.split(os.EOL);
-    res = res.map(function (line) {
-        Object.keys(this.before).forEach(function (beforeLine) {
-            if (line.indexOf(addLess(beforeLine)) > -1) {
-                line = toImports(this.before[beforeLine], 'before') + line;
-            }
-        }, this);
+  res = res.split(os.EOL)
+  res = res.map(function (line) {
+    Object.keys(this.before).forEach(function (beforeLine) {
+      if (line.indexOf(addLess(beforeLine)) > -1) {
+        line = toImports(this.before[beforeLine], 'before') + line
+      }
+    }, this)
 
-        Object.keys(this.after).forEach(function (afterLine) {
-            if (line.indexOf(addLess(afterLine)) > -1) {
-                line += toImports(this.after[afterLine], 'after');
-            }
-        }, this);
+    Object.keys(this.after).forEach(function (afterLine) {
+      if (line.indexOf(addLess(afterLine)) > -1) {
+        line += toImports(this.after[afterLine], 'after')
+      }
+    }, this)
 
-        return line;
-    }, this).join(os.EOL);
+    return line
+  }, this).join(os.EOL)
 
-    if (this.append) {
-        res += (res.slice(-1) === os.EOL ? '' : os.EOL) + this.append + (newlineAtEnd ? os.EOL : '');
-    }
+  if (this.append) {
+    res += (res.slice(-1) === os.EOL ? '' : os.EOL) + this.append + (newlineAtEnd ? os.EOL : '')
+  }
 
-    return res;
-};
+  return res
+}
 
-module.exports = LessImportInserter;
+module.exports = LessImportInserter
--- test/test.js
+++ test/test.js
@@ -1,177 +1,179 @@
-var Lab = require('lab');
-var path = require('path');
-var fs = require('fs');
-var os = require('os');
-var lab = exports.lab = Lab.script();
-var lessitizer = require('lessitizer');
-var LessImportInserter = require('..');
+var Lab = require('lab')
+var Code = require('code')
+var path = require('path')
+var fs = require('fs')
+var os = require('os')
+var lab = exports.lab = Lab.script()
+var lessitizer = require('lessitizer')
+var LessImportInserter = require('..')
 
-var bsPath = path.join(__dirname, '../node_modules/bootstrap/less/bootstrap.less');
-var bsLess = fs.readFileSync(bsPath, 'utf8');
-function toImports(line) { return !line ? '' : '@import "' + line + '";'; }
-
+var bsPath = path.join(__dirname, '../node_modules/bootstrap/less/bootstrap.less')
+var bsLess = fs.readFileSync(bsPath, 'utf8')
+function toImports (line) { return !line ? '' : '@import "' + line + '";' }
 
 lab.test('No changes', function (done) {
-    var less = new LessImportInserter({
-        lessPath: bsPath
-    }).build();
+  var less = new LessImportInserter({
+    lessPath: bsPath
+  }).build()
 
-    Lab.expect(bsLess).to.equal(less);
+  Code.expect(bsLess).to.equal(less)
 
-    done();
-});
+  done()
+})
 
 lab.test('relativeTo will change all imports', function (done) {
-    var less = new LessImportInserter({
-        lessPath: bsPath,
-        relativeTo: __dirname
-    }).build();
+  var less = new LessImportInserter({
+    lessPath: bsPath,
+    relativeTo: __dirname
+  }).build()
 
-    less.split(os.EOL).filter(function (line) {
-        return line.indexOf('@import') === 0;
-    }).forEach(function (line) {
-        Lab.expect(line.indexOf('@import "../node_modules/bootstrap/less/')).to.equal(0);
-    });
+  less.split(os.EOL).filter(function (line) {
+    return line.indexOf('@import') === 0
+  }).forEach(function (line) {
+    Code.expect(line.indexOf('@import "../node_modules/bootstrap/less/')).to.equal(0)
+  })
 
-    done();
-});
+  done()
+})
 
 lab.test('From file with newline at end', function (done) {
-    var less = new LessImportInserter({
-        lessPath: bsPath,
-        relativeTo: __dirname,
-        after: {
-            'variables.less': ['styles/my-own-variables.less', 'styles/theme.less']
-        },
-        before: {
-            'normalize': 'styles/mixins-2.less'
-        },
-        append: ['styles/this-is-the-end.less', 'styles/my-only-friend.less']
-    }).build();
+  var less = new LessImportInserter({
+    lessPath: bsPath,
+    relativeTo: __dirname,
+    after: {
+      'variables.less': ['styles/my-own-variables.less', 'styles/theme.less']
+    },
+    before: {
+      'normalize': 'styles/mixins-2.less'
+    },
+    append: ['styles/this-is-the-end.less', 'styles/my-only-friend.less']
+  }).build()
 
-    var shouldBe = [
-        '../node_modules/bootstrap/less/variables.less',
-        'styles/my-own-variables.less',
-        'styles/theme.less',
-        '../node_modules/bootstrap/less/mixins.less',
-        'styles/mixins-2.less',
-        '../node_modules/bootstrap/less/normalize.less',
-        'styles/this-is-the-end.less',
-        'styles/my-only-friend.less'
-    ];
+  var shouldBe = [
+    '../node_modules/bootstrap/less/variables.less',
+    'styles/my-own-variables.less',
+    'styles/theme.less',
+    '../node_modules/bootstrap/less/mixins.less',
+    'styles/mixins-2.less',
+    '../node_modules/bootstrap/less/normalize.less',
+    'styles/this-is-the-end.less',
+    'styles/my-only-friend.less'
+  ]
 
-    // All these should be in there
-    shouldBe.forEach(function (line) {
-        Lab.expect(toImports(line), line).to.not.equal(-1);
-    });
+  // All these should be in there
+  shouldBe.forEach(function (line) {
+    Code.expect(toImports(line), line).to.not.equal(-1)
+  })
 
-    // Checking order
-    var after = shouldBe.slice(0, 4).map(toImports).join(os.EOL) + os.EOL;
-    Lab.expect(less.indexOf(after), 'after').to.not.equal(-1);
+  // Checking order
+  var after = shouldBe.slice(0, 4).map(toImports).join(os.EOL) + os.EOL
+  Code.expect(less.indexOf(after), 'after').to.not.equal(-1)
 
-    var before = shouldBe.slice(4, 6).map(toImports).join(os.EOL) + os.EOL;
-    Lab.expect(less.indexOf(before), 'before').to.not.equal(-1);
+  var before = shouldBe.slice(4, 6).map(toImports).join(os.EOL) + os.EOL
+  Code.expect(less.indexOf(before), 'before').to.not.equal(-1)
 
-    var append = os.EOL + shouldBe.slice(-2).map(toImports).join(os.EOL) + os.EOL;
-    Lab.expect(less.indexOf(append), 'append').to.not.equal(-1);
+  var append = os.EOL + shouldBe.slice(-2).map(toImports).join(os.EOL) + os.EOL
+  Code.expect(less.indexOf(append), 'append').to.not.equal(-1)
 
-    // Newline at end of file
-    Lab.expect(less.slice(-1), 'last character newline').to.equal(os.EOL);
+  // Newline at end of file
+  Code.expect(less.slice(-1), 'last character newline').to.equal(os.EOL)
 
-    done();
-});
+  done()
+})
 
 lab.test('From string with no newline at end', function (done) {
-    var less = new LessImportInserter({
-        less: ['variables.less', 'mixins.less', null, 'normalize.less'].map(toImports).join(os.EOL),
-        after: {
-            'variables.less': ['styles/my-own-variables.less', 'styles/theme.less']
-        },
-        before: {
-            'normalize': 'styles/mixins-2.less'
-        },
-        append: ['styles/this-is-the-end.less', 'styles/my-only-friend.less']
-    }).build();
+  var less = new LessImportInserter({
+    less: ['variables.less', 'mixins.less', null, 'normalize.less'].map(toImports).join(os.EOL),
+    after: {
+      'variables.less': ['styles/my-own-variables.less', 'styles/theme.less']
+    },
+    before: {
+      'normalize': 'styles/mixins-2.less'
+    },
+    append: ['styles/this-is-the-end.less', 'styles/my-only-friend.less']
+  }).build()
 
-    var shouldBe = [
-        'variables.less',
-        'styles/my-own-variables.less',
-        'styles/theme.less',
-        'mixins.less',
-        'styles/mixins-2.less',
-        'normalize.less',
-        'styles/this-is-the-end.less',
-        'styles/my-only-friend.less'
-    ];
+  var shouldBe = [
+    'variables.less',
+    'styles/my-own-variables.less',
+    'styles/theme.less',
+    'mixins.less',
+    'styles/mixins-2.less',
+    'normalize.less',
+    'styles/this-is-the-end.less',
+    'styles/my-only-friend.less'
+  ]
 
-    shouldBe.forEach(function (line) {
-        Lab.expect(toImports(line), line).to.not.equal(-1);
-    });
+  shouldBe.forEach(function (line) {
+    Code.expect(toImports(line), line).to.not.equal(-1)
+  })
 
-    // Checking order
-    var after = shouldBe.slice(0, 4).map(toImports).join(os.EOL) + os.EOL;
-    Lab.expect(less.indexOf(after), 'after').to.not.equal(-1);
+  // Checking order
+  var after = shouldBe.slice(0, 4).map(toImports).join(os.EOL) + os.EOL
+  Code.expect(less.indexOf(after), 'after').to.not.equal(-1)
 
-    var before = shouldBe.slice(4, 6).map(toImports).join(os.EOL) + os.EOL;
-    Lab.expect(less.indexOf(before), 'before').to.not.equal(-1);
+  var before = shouldBe.slice(4, 6).map(toImports).join(os.EOL) + os.EOL
+  Code.expect(less.indexOf(before), 'before').to.not.equal(-1)
 
-    var append = os.EOL + shouldBe.slice(-2).map(toImports).join(os.EOL);
-    Lab.expect(less.indexOf(append), 'append').to.not.equal(-1);
+  var append = os.EOL + shouldBe.slice(-2).map(toImports).join(os.EOL)
+  Code.expect(less.indexOf(append), 'append').to.not.equal(-1)
 
-    // No new newline at end of file
-    Lab.expect(less.slice(-1), 'last character newline').to.not.equal(os.EOL);
+  // No new newline at end of file
+  Code.expect(less.slice(-1), 'last character newline').to.not.equal(os.EOL)
 
-    done();
-});
+  done()
+})
 
 lab.test('Bootstrap compiles after alterations', function (done) {
-    var dir = path.join(__dirname, '..');
-    var less = new LessImportInserter({
-        lessPath: bsPath,
-        relativeTo: dir,
-        after: {
-            'variables': 'fixtures/variables.less'
-        },
-        append: 'fixtures/app.less'
-    }).build();
+  var dir = path.join(__dirname, '..')
+  var less = new LessImportInserter({
+    lessPath: bsPath,
+    relativeTo: dir,
+    after: {
+      'variables': 'fixtures/variables.less'
+    },
+    append: 'fixtures/app.less'
+  }).build()
 
-    lessitizer({
-        files: {
-            less: less,
-            dir: dir
-        }
-    }, function (err, css) {
-        Lab.expect(err).to.equal(null);
-        var bodyFontFamily = [
-            'body {',
-            '  font-family: Helvetica, Arial, sans-serif;'
-        ];
-        var appCSS = [
-            '.fixture-app-content {',
-            '  color: blue;',
-            '}'
-        ];
-        [bodyFontFamily, appCSS].forEach(function (shouldBe) {
-            Lab.expect(css[0].indexOf(shouldBe.join('\n'))).to.not.equal(-1);
-        });
-        done();
-    });
-});
+  lessitizer({
+    files: {
+      less: less,
+      dir: dir
+    }
+  }, function (err, css) {
+    Code.expect(err).to.equal(null)
+    var bodyFontFamily = [
+      'body {',
+      '  font-family: Helvetica, Arial, sans-serif;'
+    ]
+    var appCSS = [
+      '.fixture-app-content {',
+      '  color: blue;',
+      '}'
+    ];
+    [bodyFontFamily, appCSS].forEach(function (shouldBe) {
+      Code.expect(css[0].indexOf(shouldBe.join('\n'))).to.not.equal(-1)
+    })
+    done()
+  })
+})
 
 lab.experiment('Errors', function () {
-    lab.test('Error with no options', function (done) {
-        Lab.expect(function () {
-            new LessImportInserter();
-        }).to.throw(Error);
+  lab.test('Error with no options', function (done) {
+    Code.expect(function () {
+      // eslint-disable-next-line no-new
+      new LessImportInserter()
+    }).to.throw(Error)
 
-        done();
-    });
+    done()
+  })
 
-    lab.test('Error with insufficient options', function (done) {
-        Lab.expect(function () {
-            new LessImportInserter({});
-        }).to.throw(Error);
+  lab.test('Error with insufficient options', function (done) {
+    Code.expect(function () {
+      // eslint-disable-next-line no-new
+      new LessImportInserter({})
+    }).to.throw(Error)
 
-        done();
-    });
-});
+    done()
+  })
+})
